d91ca79b01a4cb0231210ef6904bc8f78f8edc7a
tm-a-t <tmat.ivanov@gmail.com>
2023-10-22 03:15:29 +0500

    fix analytics

--- .vitepress/config/head.ts
+++ .vitepress/config/head.ts
@@ -1,6 +1,6 @@
 import {HeadConfig} from 'vitepress/types/shared'
 
-const metrikaScript = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(95150339, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });`
+const metrikaScript = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(95150339, "init", { defer:true, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });`
 const metrikaNoscript = `<div><img src="https://mc.yandex.ru/watch/95150339" style="position:absolute; left:-9999px;" alt="" /></div>`
 
 const head: HeadConfig[] = [
--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme';
+import {useRouter} from 'vitepress';
 
-const { Layout } = DefaultTheme
+const {Layout} = DefaultTheme;
+const router = useRouter();
+
+router.onAfterRouteChanged = async (to: string) => {
+  if (!import.meta.env.SSR) {
+    ym(95150339, 'hit', to);
+  }
+}
 </script>
 
 <template>
