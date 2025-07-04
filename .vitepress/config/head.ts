f5f9c864ef5c0f2bc7a546ade6c92f1a617f4317
Artem Ivanov <tmat.ivanov@gmail.com>
2025-07-04 12:45:57 +0300

    Add Umami analytics

--- .vitepress/config/head.ts
+++ .vitepress/config/head.ts
@@ -1,4 +1,5 @@
 import {HeadConfig} from 'vitepress'
+import {env} from "node:process";
 
 const head: HeadConfig[] = [
     ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'}],
@@ -6,5 +7,12 @@ const head: HeadConfig[] = [
     ['link', {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'}],
     ['link', {rel: 'manifest', href: '/site.webmanifest'}],
 ]
+
+if (env.NODE_ENV === 'production') {
+    // Use Umami analytics
+    head.push(
+        ['script', {defer: 'true', src: 'https://zond.vanutp.dev/script.js', 'data-website-id': 'b055fd15-b1a0-4d26-a389-01026049323a'}],
+    )
+}
 
 export default head
